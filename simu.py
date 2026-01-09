13db6228c565a413057c155cba2647e7ce6f1a05
unknown <sorag.eth@protonmail.com>
2026-01-09 23:23:05 +0100

    AV

--- simu.py
+++ simu.py
@@ -4,31 +4,31 @@ import numpy as np
 # https://www.service-public.gouv.fr/particuliers/vosdroits/F35794
 
 # Son / grand son / parents => FIRST DEGREE
-TAX_DIRECT_DEDUCTION = 100000
+TAX_DIRECT_DEDUCTION = 100_000
 TAX_DIRECT_5 = 0.05
-TAX_DIRECT_5_MAX = 8072
+TAX_DIRECT_5_MAX = 8_072
 TAX_DIRECT_10 = 0.1
-TAX_DIRECT_10_MAX = 12109
+TAX_DIRECT_10_MAX = 12_109
 TAX_DIRECT_15 = 0.15
-TAX_DIRECT_15_MAX = 15932
+TAX_DIRECT_15_MAX = 15_932
 TAX_DIRECT_20 = 0.2
-TAX_DIRECT_20_MAX = 552324
+TAX_DIRECT_20_MAX = 552_324
 TAX_DIRECT_30 = 0.3
-TAX_DIRECT_30_MAX = 902838
+TAX_DIRECT_30_MAX = 902_838
 TAX_DIRECT_40 = 0.4
-TAX_DIRECT_40_MAX = 1805677
+TAX_DIRECT_40_MAX = 1_805_677
 TAX_DIRECT_45 = 0.45
 
 # Brother / Sister
-TAX_BS_DEDUCTION = 15932
+TAX_BS_DEDUCTION = 15_932
 TAX_BS_35 = 0.35
-TAX_BS_35_MAX = 24430
+TAX_BS_35_MAX = 24_430
 TAX_BS_45 = 0.45
 
 # AV
-TAX_AV_DEDUCTION = 152500
+TAX_AV_DEDUCTION = 152_500
 TAX_AV_20 = 0.2
-TAX_AV_20_MAX = 700000
+TAX_AV_20_MAX = 700_000
 TAX_AV_3125 = 0.3125
 
 # Return tax for the given capital with direct bloodline
@@ -86,5 +86,23 @@ def tax_first_degree(capital) -> float:
     # 45
     else:
         result = (capital-TAX_BS_35_MAX-TAX_BS_DEDUCTION)*TAX_BS_45 + TAX_BS_35_MAX*TAX_BS_35
+
+    return round(result, 2)
+
+def tax_av(capital) -> float:
+
+    results = 0.0
+
+    # 0
+    if capital <= TAX_AV_DEDUCTION:
+        result = 0.0
+    
+    # 20
+    elif TAX_AV_DEDUCTION < capital and capital <= TAX_AV_DEDUCTION + TAX_AV_20_MAX:
+        result = (capital-TAX_AV_DEDUCTION)*TAX_AV_20
+    
+    # 31.25
+    else:
+        result = (capital-TAX_AV_20_MAX-TAX_AV_DEDUCTION)*TAX_AV_3125 + TAX_AV_20_MAX*TAX_AV_20
 
     return round(result, 2)
--- test.py
+++ test.py
@@ -1,27 +1,35 @@
-from simu import tax_direct, tax_first_degree
+from simu import tax_direct, tax_first_degree, tax_av
 
 # https://www.service-public.gouv.fr/simulateur/calcul/droits-succession#main
 
 def test_tax_direct():
 
     assert tax_direct(0) == 0
-    assert tax_direct(50000) == 0
-    assert tax_direct(100000) == 0
-    assert tax_direct(105000) == 250
-    assert tax_direct(108072) == 403.6
-    assert tax_direct(109000) == 496.4
-    assert tax_direct(113000) == 940.95
-    assert tax_direct(200000) == 18194.35
-    assert tax_direct(700000) == 122961.95
-    assert tax_direct(1500000) == 412678.15
-    assert tax_direct(2000000) == 617394.3
+    assert tax_direct(50_000) == 0
+    assert tax_direct(100_000) == 0
+    assert tax_direct(105_000) == 250
+    assert tax_direct(108_072) == 403.6
+    assert tax_direct(109_000) == 496.4
+    assert tax_direct(113_000) == 940.95
+    assert tax_direct(200_000) == 18_194.35
+    assert tax_direct(700_000) == 122_961.95
+    assert tax_direct(1_500_000) == 412_678.15
+    assert tax_direct(2_000_000) == 617_394.3
 
 def test_second_degree():
 
     assert tax_first_degree(0) == 0
-    assert tax_first_degree(15000) == 0
-    assert tax_first_degree(16000) == 23.8
-    assert tax_first_degree(20000) == 1423.8
-    assert tax_first_degree(30000) == 4923.8
-    assert tax_first_degree(50000) == 12887.6
-    assert tax_first_degree(100000) == 35387.6
+    assert tax_first_degree(15_000) == 0
+    assert tax_first_degree(16_000) == 23.8
+    assert tax_first_degree(20_000) == 1423.8
+    assert tax_first_degree(30_000) == 4923.8
+    assert tax_first_degree(50_000) == 12_887.6
+    assert tax_first_degree(100_000) == 35_387.6
+
+def test_av():
+    assert tax_av(0) == 0
+    assert tax_av(152_500) == 0
+    assert tax_av(252_500) == 20_000
+    assert tax_av(352_500) == 40_000
+    assert tax_av(852_500) == 140_000
+    assert tax_av(1_000_000) == 186_093.75
